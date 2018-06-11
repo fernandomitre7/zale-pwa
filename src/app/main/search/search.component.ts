acf81ee9f430c4eba1ba5e31be8c23174e22f444
Jose Fernando Mitre Alvarez <fernandomitre7@gmail.com>
2018-06-11 14:01:05 -0500

    Started working on search result list. Need ripple effect on tap Need search bar to go all the way to the top hidding header

--- src/app/main/search/search.component.ts
+++ src/app/main/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UIService } from '../../core/ui/ui.service';
 
 @Component({
     selector: 'app-search',
@@ -8,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class SearchComponent implements OnInit {
 
     private inputFocused: boolean;
-    constructor() { }
+
+    results = [
+        { type: 'restaurant', icon: 'restaurant', name: 'Il Gardiano', description: 'En Il Mercato Gentiloni' },
+        { type: 'library', icon: 'local_library', name: 'Gandhi', description: 'Tu pones las ganas de leer' },
+        { type: 'generic-store', icon: 'store', name: 'Tiendita', description: 'De la esquina' },
+        { type: 'market', icon: 'shopping_cart', name: 'Supermercado', description: 'Productos locales' },
+        { type: 'cafe', icon: 'local_cafe', name: 'Venenito Café', description: '¿Apoco no se te antoja un cafecito?' },
+        { type: 'bar', icon: 'local_bar', name: 'La Cantina', description: 'En Il Mercato Gentiloni' },
+        { type: 'apparel', icon: 'local_offer', name: 'Tienda de ropa', description: 'Estamos en oferta' },
+        { type: 'fastfood', icon: 'fastfood', name: 'Hamburguesas al carbón', description: 'Las mejores de Saltillo' },
+        { type: 'pizzeria', icon: 'local_pizza', name: 'Capricciosas Rufino Tamayo', description: 'Pizza Gourmet' },
+        { type: 'gas-station', icon: 'local_gas_station', name: 'Oxxo Gas', description: 'Pioneros' },
+
+        { type: 'restaurant', icon: 'restaurant', name: 'Il Gardiano', description: 'En Il Mercato Gentiloni' },
+        { type: 'library', icon: 'local_library', name: 'Gandhi', description: 'Tu pones las ganas de leer' },
+        { type: 'generic-store', icon: 'store', name: 'Tiendita', description: 'De la esquina' },
+        { type: 'market', icon: 'shopping_cart', name: 'Supermercado', description: 'Productos locales' },
+        { type: 'cafe', icon: 'local_cafe', name: 'Venenito Café', description: '¿Apoco no se te antoja un cafecito?' },
+        { type: 'bar', icon: 'local_bar', name: 'La Cantina', description: 'En Il Mercato Gentiloni' },
+        { type: 'apparel', icon: 'local_offer', name: 'Tienda de ropa', description: 'Estamos en oferta' },
+        { type: 'fastfood', icon: 'fastfood', name: 'Hamburguesas al carbón', description: 'Las mejores de Saltillo' },
+        { type: 'pizzeria', icon: 'local_pizza', name: 'Capricciosas Rufino Tamayo', description: 'Pizza Gourmet' },
+        { type: 'gas-station', icon: 'local_gas_station', name: 'Oxxo Gas', description: 'Pioneros' },
+    ];
+
+    constructor(private uiService: UIService) { }
 
     ngOnInit() {
         // this.inputFocused = true; //
@@ -16,6 +42,7 @@ export class SearchComponent implements OnInit {
 
     onSearchFocus() {
         this.inputFocused = true;
+        this.uiService.inputFocused();
     }
 
     unFocusSearch() {
